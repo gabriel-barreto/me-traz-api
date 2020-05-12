3a69c8f29ec18c5b7675a81cfa09679b06e6fdcf
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-05-12 04:51:51 -0300

    fix(controllers): barrel imports

--- src/Controllers/index.ts
+++ src/Controllers/index.ts
@@ -1,3 +1,3 @@
-export { default as order } from './order.model'
-export { default as product } from './product.model'
+export { default as order } from './order.controller'
+export { default as product } from './product.controller'
 export { default as root } from './root.controller'
